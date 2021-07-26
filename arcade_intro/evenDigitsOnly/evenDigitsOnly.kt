1f300ffcaf6fc9a6877eafab8192968d7fa94c46
ZAINAB MAHMOOD <36710643+zainab9271@users.noreply.github.com>
2021-07-26 16:48:07 +0300

    Update evenDigitsOnly.kt

--- arcade_intro/evenDigitsOnly/evenDigitsOnly.kt
+++ arcade_intro/evenDigitsOnly/evenDigitsOnly.kt
@@ -5,6 +5,6 @@ package evenDigitsOnly
  * @author Zainab Mahmood
  * @param n A one-digit or multi-digit integer
  * @return True if all digits are even or False otherwise.
- * @see []
+ * @see [evenDigitsOnly/evenDigitsOnly_description.md]
  */
 fun evenDigitsOnly(n: Int) = n.toString().filter { it in '1'..'9' step 2 } == ""
